bae13c128ed27657e34a70f6a41a3d9bfbcafb11
rputramadhan <88891302+rputramadhan@users.noreply.github.com>
2021-08-14 15:59:09 +0700

    Update footer.php

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,13 +1,11 @@
-    <script src="../vendor/jquery/jquery.min.js"></script>
-    <script src="../vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-    <script src="../vendor/jquery-easing/jquery.easing.min.js"></script>
-    <script src="../js/sb-admin-2.min.js"></script>
-    <script src="vendor/datatables/jquery.dataTables.min.js"></script>
-    <script src="vendor/datatables/dataTables.bootstrap4.min.js"></script>
-    <script src="../vendor/chart.js/Chart.min.js"></script>
-    <script src="../js/demo/chart-area-demo.js"></script>
-    <script src="../js/demo/chart-pie-demo.js"></script>
-    <script src="../js/demo/datatables-demo.js"></script>
+    <script src="<?= base_url() ?>vendor/jquery/jquery.min.js"></script>
+    <script src="<?= base_url() ?>vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="<?= base_url() ?>vendor/jquery-easing/jquery.easing.min.js"></script>
+    <script src="<?= base_url() ?>js/sb-admin-2.min.js"></script>
+    <script src="<?= base_url() ?>vendor/chart.js/Chart.min.js"></script>
+    <script src="<?= base_url() ?>js/demo/chart-area-demo.js"></script>
+    <script src="<?= base_url() ?>js/demo/chart-pie-demo.js"></script>
+    <script src="<?= base_url() ?>js/demo/datatables-demo.js"></script>
 
 </body>
 
